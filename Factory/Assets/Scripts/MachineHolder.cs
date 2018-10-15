b51f701b97acea6a97aff1327dc70dca677bf9ea
Sven Fröhling <sven.froehling@outlook.de>
2018-10-16 00:08:54 +0700

    Added the Overlay for arrow

---
+++ Factory/Assets/Scripts/MachineHolder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineHolder : Singleton<MachineHolder> {
+
+    public void ActivateRotationMode()
+    {
+        foreach (Transform child in transform)
+        {
+            if(child.tag == "machineTag")
+            {
+                child.GetChild(1).gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public void DeactivateRotationMode()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.tag == "machineTag")
+            {
+                child.GetChild(1).gameObject.SetActive(false);
+            }
+        }
+    }
+}

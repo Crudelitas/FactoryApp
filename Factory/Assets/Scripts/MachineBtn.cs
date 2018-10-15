193dedc7728ae82626d3de051779972f5dc52468
Unknown <sven.froehling@outlook.de>
2018-10-15 13:30:17 +0700

    Add Unity Project

---
+++ Factory/Assets/Scripts/MachineBtn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineBtn : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject machine;
+
+    public GameObject Machine
+    {
+        get
+        {
+            return machine;
+        }
+    }
+}
---
+++ Factory/Assets/Scripts/Roller.cs
@@ -0,0 +1,12 @@
+﻿
+public class Roller : Machine {
+
+    private void Update()
+    {
+        
+    }
+
+    public override void Process(){
+        print("Hello I am Working");
+    }
+}
